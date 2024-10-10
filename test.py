14923e0769771dfb85ee4899e6df477942215dbb
lonelygunter <89039096+lonelygunter@users.noreply.github.com>
2024-10-10 21:38:21 +0200

    update test.py (indexes)

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
 import pickle
+import sys
 
 ### Constants
-TRAINED_MODEL_PATH = "./tsd_30epochs/model_trained_30epochs.p" # Set the trained model path
 FRAME_WIDTH = 640 # Set width of the video frame
 FRAME_HEIGHT = 480 # Set height of the video frame
 BRIGHTNESS = 180 # Set brightness level
@@ -37,43 +37,38 @@ def getCalssName(classNo):
     elif classNo == 3: return 'Speed Limit 60 km/h'
     elif classNo == 4: return 'Speed Limit 70 km/h'
     elif classNo == 5: return 'Speed Limit 80 km/h'
-    elif classNo == 6: return 'End of Speed Limit 80 km/h'
-    elif classNo == 7: return 'Speed Limit 100 km/h'
-    elif classNo == 8: return 'Speed Limit 120 km/h'
-    elif classNo == 9: return 'No passing'
-    elif classNo == 10: return 'No passing for vehicles over 3.5 metric tons'
-    elif classNo == 11: return 'Right-of-way at the next intersection'
-    elif classNo == 12: return 'Priority road'
-    elif classNo == 13: return 'Yield'
-    elif classNo == 14: return 'Stop'
-    elif classNo == 15: return 'No vehicles'
-    elif classNo == 16: return 'Vehicles over 3.5 metric tons prohibited'
-    elif classNo == 17: return 'No entry'
-    elif classNo == 18: return 'General caution'
-    elif classNo == 19: return 'Dangerous curve to the left'
-    elif classNo == 20: return 'Dangerous curve to the right'
-    elif classNo == 21: return 'Double curve'
-    elif classNo == 22: return 'Bumpy road'
-    elif classNo == 23: return 'Slippery road'
-    elif classNo == 24: return 'Road narrows on the right'
-    elif classNo == 25: return 'Road work'
-    elif classNo == 26: return 'Traffic signals'
-    elif classNo == 27: return 'Pedestrians'
-    elif classNo == 28: return 'Children crossing'
-    elif classNo == 29: return 'Bicycles crossing'
-    elif classNo == 30: return 'Beware of ice/snow'
-    elif classNo == 31: return 'Wild animals crossing'
-    elif classNo == 32: return 'End of all speed and passing limits'
-    elif classNo == 33: return 'Turn right ahead'
-    elif classNo == 34: return 'Turn left ahead'
-    elif classNo == 35: return 'Ahead only'
-    elif classNo == 36: return 'Go straight or right'
-    elif classNo == 37: return 'Go straight or left'
-    elif classNo == 38: return 'Keep right'
-    elif classNo == 39: return 'Keep left'
-    elif classNo == 40: return 'Roundabout mandatory'
-    elif classNo == 41: return 'End of no passing'
-    elif classNo == 42: return 'End of no passing by vehicles over 3.5 metric tons'
+    elif classNo == 6: return 'Speed Limit 100 km/h'
+    elif classNo == 7: return 'Speed Limit 120 km/h'
+    elif classNo == 8: return 'No passing'
+    elif classNo == 9: return 'Right-of-way at the next intersection'
+    elif classNo == 10: return 'Priority road'
+    elif classNo == 11: return 'Yield'
+    elif classNo == 12: return 'Stop'
+    elif classNo == 13: return 'No vehicles'
+    elif classNo == 14: return 'Vehicles over 3.5 metric tons prohibited'
+    elif classNo == 15: return 'No entry'
+    elif classNo == 16: return 'General caution'
+    elif classNo == 17: return 'Dangerous curve to the left'
+    elif classNo == 18: return 'Dangerous curve to the right'
+    elif classNo == 19: return 'Double curve'
+    elif classNo == 20: return 'Bumpy road'
+    elif classNo == 21: return 'Slippery road'
+    elif classNo == 22: return 'Road narrows on the right'
+    elif classNo == 23: return 'Road work'
+    elif classNo == 24: return 'Pedestrians'
+    elif classNo == 25: return 'Children crossing'
+    elif classNo == 26: return 'Bicycles crossing'
+    elif classNo == 27: return 'Beware of ice/snow'
+    elif classNo == 28: return 'Wild animals crossing'
+    elif classNo == 29: return 'End of all speed and passing limits'
+    elif classNo == 30: return 'Turn right ahead'
+    elif classNo == 31: return 'Turn left ahead'
+    elif classNo == 32: return 'Ahead only'
+    elif classNo == 33: return 'Go straight or right'
+    elif classNo == 34: return 'Go straight or left'
+    elif classNo == 35: return 'Keep right'
+    elif classNo == 36: return 'Keep left'
+    elif classNo == 37: return 'Roundabout mandatory'
 
 ### Main
 
@@ -83,9 +78,15 @@ video_cap.set(3, FRAME_WIDTH) # Set the width of the frame
 video_cap.set(4, FRAME_HEIGHT) # Set the height of the frame
 video_cap.set(10, BRIGHTNESS) # Set the brightness of the frame
 
+# take path from command line argument
+trained_model_path = sys.argv[1]
+while not trained_model_path:
+    trained_model_path = input("Enter the path to the trained model: ")
+
 # Load the trained model using pickle
-pickle_in = open(TRAINED_MODEL_PATH, "rb")
+pickle_in = open(trained_model_path, "rb")
 model = pickle.load(pickle_in)
+last_class = "" # Store the last class index
 
 while True:
     # Capture a frame from the camera
@@ -93,7 +94,7 @@ while True:
     flipped_frame = cv2.flip(original_frame, 1)
     
     # Convert the captured frame to a numpy array and resize it
-    img = np.asarray(flipped_frame)
+    img = np.asarray(original_frame)
     img = cv2.resize(img, IMG_DIM_RES) # Resize to 32x32 pixels
 
     # Preprocess the image
@@ -106,6 +107,7 @@ while True:
     # Display placeholder text for classification and probability on the original image
     cv2.putText(flipped_frame, "CLASS: ", (20, 35), FONT, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
     cv2.putText(flipped_frame, "PROBABILITY: ", (20, 75), FONT, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+    cv2.putText(flipped_frame, "LAST: ", (20, 115), FONT, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
 
     # Make predictions using the model
     predictions = model.predict(img)
@@ -119,6 +121,9 @@ while True:
         # Display the class name and probability on the original image
         cv2.putText(flipped_frame, str(class_id) + " " + str(class_name), (120, 35), FONT, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
         cv2.putText(flipped_frame, str(round(probability_value * 100, 2)) + "%", (180, 75), FONT, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
+        last_class = str(class_id) + " " + str(class_name)
+
+    cv2.putText(flipped_frame, str(last_class), (100, 115), FONT, 0.75, (0, 0, 255), 2, cv2.LINE_AA)
     
     # Show the result image with the class and probability
     cv2.imshow("Result", flipped_frame)
