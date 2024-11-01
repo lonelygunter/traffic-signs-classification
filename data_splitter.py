4f8b04e266f4e62aca3c9ecb0d9f3b18acf7855c
lonelygunter <89039096+lonelygunter@users.noreply.github.com>
2024-11-01 23:34:17 +0100

    Update data_splitter.py

--- data_splitter.py
+++ data_splitter.py
@@ -3,9 +3,11 @@ import re
 import shutil
 
 # Define the path to the train data directory
-train_data_dir = './dataset/train_data'
-val_data_dir = './dataset/val_data'
-test_data_dir = './dataset/test_data'
+TRAIN_DATA_DIR = './dataset_no0_0.2/train_data'
+VAL_DATA_DIR = './dataset_no0_0.2/val_data'
+TEST_DATA_DIR = './dataset_no0_0.2/test_data'
+VAL_PROP = 0.2
+TEST_PROP = 0.2
 
 def extract_number(filename):
     """ Extracts the last number from a filename """
@@ -14,13 +16,13 @@ def extract_number(filename):
     return int(match.group(1)) if match else float('inf')  # Return inf if no match found
 
 # Loop through each class folder in the train directory
-for class_dir in os.listdir(train_data_dir):
-    class_path = os.path.join(train_data_dir, class_dir)
+for class_dir in os.listdir(TRAIN_DATA_DIR):
+    class_path = os.path.join(TRAIN_DATA_DIR, class_dir)
     
     if os.path.isdir(class_path):
         # Create class-specific directories in val_data and test_data
-        val_class_dir = os.path.join(val_data_dir, class_dir)
-        test_class_dir = os.path.join(test_data_dir, class_dir)
+        val_class_dir = os.path.join(VAL_DATA_DIR, class_dir)
+        test_class_dir = os.path.join(TEST_DATA_DIR, class_dir)
         
         os.makedirs(val_class_dir, exist_ok=True)
         os.makedirs(test_class_dir, exist_ok=True)
@@ -28,18 +30,25 @@ for class_dir in os.listdir(train_data_dir):
         # Get a list of all image files in the class directory
         images = [img for img in os.listdir(class_path) if img.endswith(('jpg', 'jpeg', 'png'))]
 
+        # Get number of images in the train directory
+        num_images = len(images)
+
+        # Calculate the number of images to move to the validation and test directories
+        num_val_images = int(num_images * VAL_PROP)
+        num_test_images = int(num_images * TEST_PROP)
+
         # Sort the files based on the extracted number
         images = sorted(images, key=extract_number)
 
         # Move the first 10 images to the validation folder
-        for img in images[:10]:
+        for img in images[:num_val_images]:
             src = os.path.join(class_path, img)
             dest = os.path.join(val_class_dir, img)
             shutil.move(src, dest)
             print(f'Moved {src} to {dest}')
 
         # Move the next 10 images to the test folder
-        for img in images[-10:]:  # Moving the next 10 images
+        for img in images[-num_test_images:]:  # Moving the next 10 images
             src = os.path.join(class_path, img)
             dest = os.path.join(test_class_dir, img)
             if os.path.exists(src):  # Check if the file still exists
